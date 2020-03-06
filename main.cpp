4dcf70a28abbea2e1754bfcb6dd4e5073305422e
kingskingsjin@126.com <zhaojin518>
2020-03-06 16:50:40 +0800

    优化

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <sys/time.h>
 
+
 struct Transaction {
     int id;
-    char *info;
+    char info[32];
 };
 
 struct Block {
     int index;
-    char *hash;
-    char *previous_hash;
+    char hash[32];
+    char previous_hash[32];
     long timestamp;
     int nonce;
     struct Transaction transactions[2];
@@ -17,22 +18,21 @@ struct Block {
 
 struct Block block;
 
-
 int main() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
 
     block.index = 1;
-    block.hash = "00c01b629d0d0fba452a0a632945f1f0fd1dd92a864d102112f8ccde3eaf516b";
-    block.previous_hash = "00ced8f1a7407460521d279364db6d48ccb8aa3ad839ea08220d7e8347d276ef";
+    strcpy(block.hash, "00c01b629d0d0fba452a0a632945f1f0fd1dd92a864d102112f8ccde3eaf516b");
+    strcpy(block.previous_hash, "00ced8f1a7407460521d279364db6d48ccb8aa3ad839ea08220d7e8347d276ef");
     block.timestamp = tv.tv_sec;
     block.nonce = 1;
     block.transactions[0].id = 1;
-    block.transactions[0].info = "共识机制生成的区块";
+    strcpy(block.transactions[0].info, "共识机制生成的区块");
     block.transactions[1].id = 2;
-    block.transactions[1].info = "区块链高度为2";
+    strcpy(block.transactions[1].info, "区块链高度为2");
 
-    printf("%d %s", block.timestamp, block.transactions[0].info);
+    printf("%d %s", block.timestamp, block.transactions[1].info);
 
     return 0;
 }
