3ce7a90045e52517870deed702b6960ef0610976
kingskingsjin@126.com <zhaojin518>
2020-03-07 17:20:04 +0800

    init

---
+++ block.h
@@ -0,0 +1,34 @@
+//
+// Created by allnas on 2020/3/7.
+//
+
+#ifndef FIRST_C_COMMON_H
+#define FIRST_C_COMMON_H
+
+#endif //FIRST_C_COMMON_H
+
+struct Transaction {
+    int id;
+    char* info;
+};
+
+struct Block {
+    int index;
+    char* hash;
+    char* previous_hash;
+    long timestamp;
+    int nonce;
+    struct Transaction transactions[1];
+};
+
+struct Block block;
+
+/**
+ * 函数声明
+ * @param block
+ */
+void printBlock(struct Block block);
+
+void printBlock(struct Block block){
+    printf("%ld %s", block.timestamp, block.transactions[0].info);
+}
